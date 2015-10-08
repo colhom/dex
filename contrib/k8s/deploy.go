f12a39f501a629e1e94f445d603943e05f669588
Colin Hom <colhom@gmail.com>
2015-10-07 19:15:44 -0700

    [contrib/k8s] yaml files are now thrown at kubectl at the same time there are no steps that have to happen "in between" pods going up the only logic left in deploy.go is   -accepting args   -templating the yaml files with those args   -putting the results in the gen folder   -kubectl delete -f ./gen   -kubectl create -f ./gen
    no more polling the kube API for pod state!!!!

--- contrib/k8s/deploy.go
+++ contrib/k8s/deploy.go
@@ -8,8 +8,8 @@ import (
 	"os/exec"
 	"path"
 	"text/template"
-	"time"
 	"encoding/base64"
+	"regexp"
 )
 
 type DexKubeParams struct {
@@ -79,12 +79,20 @@ func main() {
 		panic(err)
 	}
 
+    postgresRegexp := regexp.MustCompile("^postgres-.*.yaml$")
+
 	for i := range yamlFiles {
 		inpath := yamlFiles[i].Name()
 
 		if path.Ext(inpath) != ".yaml" {
 			continue
 		}
+
+        if !deployPostgres && postgresRegexp.MatchString(path.Base(inpath)) {
+            // if we're NOT deploying postgres, and this is a postgres-*.yaml file -->
+            // then skip it
+            continue
+        }
 
 		genpath := path.Join(".", GEN_FOLDER, path.Base(inpath))
 
@@ -93,103 +101,19 @@ func main() {
 		}
 	}
 
-	if _, err := retryCmd(5, 3, "kubectl", "get", "nodes"); err != nil {
-		panic(err)
-	}
+    genPath := path.Join(".","gen")
 
-    postgresFiles := [2]string{"postgres-rc.yaml","postgres-service.yaml"}
-
-	overlordFiles := [3]string{"dex-secrets.yaml", "dex-overlord-rc.yaml", "dex-overlord-service.yaml"}
-
-	workerFiles := [2]string{"dex-worker-rc.yaml", "dex-worker-service.yaml"}
-
-    //Begin pod cleanup
-
-	for i := range workerFiles {
-		f := workerFiles[i]
-		_ = exec.Command("kubectl", "delete", "-f", path.Join(".", "gen", f)).Run()
-	}
-
-	for i := range overlordFiles {
-		f := overlordFiles[i]
-		_ = exec.Command("kubectl", "delete", "-f", path.Join(".", "gen", f)).Run()
-	}
-
-    if deployPostgres {
-        for i := range postgresFiles {
-            f := postgresFiles[i]
-            _ = exec.Command("kubectl", "delete", "-f", path.Join(".", "gen", f)).Run()
-        }
+    if output,err := exec.Command("kubectl","delete","-f",genPath).CombinedOutput(); err != nil {
+        fmt.Printf("output: %s\n",output)
+        panic(err)
     }
 
-    //End pod cleanup
-    //Begin pod creation
-    if deployPostgres {
-        for i := range postgresFiles {
-            f := postgresFiles[i]
-
-            if _, err := doCmd("kubectl", "create", "-f", path.Join(".", "gen", f)); err != nil {
-                panic(err)
-            }
-        }
-
-        if psqlPod,err := doCmd("kubectl","get","pod" ,"-l=app=postgres","-o","template", "-t","{{ (index .items 0).metadata.name }}"); err != nil {
-
-            panic(err)
-
-        }else{
-            if _,err := retryCmd(20,6,"kubectl","exec",string(psqlPod),"--","psql","-c","\\list",params.PostgresUrl); err != nil {
-                panic(err)
-            }
-        }
-    }
-
-	for i := range overlordFiles {
-		f := overlordFiles[i]
-		if _, err := doCmd("kubectl", "create", "-f", path.Join(".", "gen", f)); err != nil {
-			panic(err)
-		}
-	}
-
-    for i := range(workerFiles){
-        if _,err := doCmd("kubectl","create","-f",path.Join(".","gen",workerFiles[i])); err != nil {
-            panic(err)
-        }
+    if output,err := exec.Command("kubectl","create","-f",genPath).CombinedOutput(); err != nil {
+        fmt.Printf("output: %s\n",output)
+        panic(err)
     }
 }
 
-func retryCmd(maxAttempts, waitSec int, name string, args ...string) (output []byte, err error) {
-
-	for i := 0; i < maxAttempts; i++ {
-        log("(%d/%d):\n", i+1, maxAttempts)
-
-		if output, err = doCmd(name, args...); err != nil {
-			log("\terror: %s\n",err.Error())
-
-            if i < maxAttempts - 1 {
-                time.Sleep(time.Second*time.Duration(waitSec))
-            }else{
-                fmt.Printf("%s %v\n\t->fatal error after %d tries: %s\n",name,args,i+1,err.Error())
-            }
-
-		} else {
-			log("\t->OK\n")
-			return
-		}
-	}
-
-	return
-}
-
-func doCmd(name string, args ...string) (output []byte, err error) {
-	output, err = exec.Command(name, args...).CombinedOutput()
-
-	log("$> %s %v\n\n", name, args)
-
-	log("%s\n", string(output))
-
-	return
-}
 func templateYamlFile(params DexKubeParams, inpath, genpath string) error {
 	if tmpl, err := template.New(path.Base(inpath)).ParseFiles(inpath); err != nil {
 
